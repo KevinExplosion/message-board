10571c3287ba4d7a2aa969df4f4a56273b067c60
kevin <mattisonii@yahoo.com>
2016-04-01 09:33:27 -0700

    Create new question form and cancel question function.

---
+++ app/components/new-question.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+    questionFormHide() {
+      this.set('addNewQuestion', false);
+    }
+  }
+});
