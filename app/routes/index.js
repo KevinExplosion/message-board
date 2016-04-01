c16b69cbfc78ab295790f0e6687f72cabc3f5882
kevin <mattisonii@yahoo.com>
2016-04-01 11:59:48 -0700

    Create answers model.

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('questions');
+    return Ember.RSVP.hash({
+      questions: this.store.findAll('questions'),
+      answers: this.store.findAll('answers')
+    });
   },
 
   actions: {
@@ -11,20 +14,5 @@ export default Ember.Route.extend({
       newQuestion.save();
       this.transitionTo('index');
     }
-
-    // update(question, params) {
-    //   Object.keys(params).forEach(function(key) {
-    //     if(params[key]!==undefined) {
-    //       question.set(key,params[key]);
-    //     }
-    //   });
-    //   question.save();
-    //   this.transitionTo('index');
-    // },
-    //
-    // destroyQuestion(question) {
-    //   question.destroyRecord();
-    //   this.transitionTo('index');
-    // }
   }
 });
