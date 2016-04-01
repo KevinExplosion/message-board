42b1b42ebeb00fdeb081f07ddec194301687da7b
kevin <mattisonii@yahoo.com>
2016-04-01 13:51:33 -0700

    Add background styling, margins, and padding.

--- app/models/answers.js
+++ app/models/answers.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  title: DS.attr(),
+  title: DS.belongsTo('questions', { async: true}),
   author: DS.attr(),
   body: DS.attr()
 });
