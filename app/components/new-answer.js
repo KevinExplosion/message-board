732cc4dc6193e195270d96d7b3b14c90eadfc864
kevin <mattisonii@yahoo.com>
2016-04-05 17:58:10 -0700

    Create route handing for show/hide on answer form.

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewAnswer: false,
+  actions: {
+    answerFormShow() {
+      this.set('addNewAnswer', true);
+    },
+
+    answerFormHide() {
+      this.set('addNewAnswer', false)
+    }
+  }
 });
