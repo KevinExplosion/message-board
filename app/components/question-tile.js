0425f13165a58d23fb6095da2738943b780f55a9
kevin <mattisonii@yahoo.com>
2016-04-14 16:01:04 -0700

    recreate computer property.

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   isBodyShowing: false,
+
+  fullQuestionTitle: Ember.computed('question.title', 'question.author', function() {
+    return this.get('question.title') + ', by: ' + this.get('question.author');
+  }),
+  
   actions: {
     delete(question) {
       if(confirm('Would you like to delete this question from the board?')){
